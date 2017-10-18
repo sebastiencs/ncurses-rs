4da8b26fd0cf7665bdfc8c526c734aa5a33da6ae
Sebastien Chapuis <sebastien@chapu.is>
2017-10-18 17:49:36 +0200

    implementing form

--- src/form/wrapper.rs
+++ src/form/wrapper.rs
@@ -7,6 +7,8 @@ use constants::TRUE;
 use std::ptr;
 use std::cmp::PartialEq;
 
+use libc::c_int;
+
 pub type FORM = ll::FORM;
 pub type FIELD = ll::FIELD;
 pub type FIELDTYPE = ll::FIELDTYPE;
@@ -35,7 +37,7 @@ pub enum FormCode {
 trait Form_Result {
     fn from_code(_: i32) -> FormCode;
     fn result(_: i32) -> FormResult;
-    fn result_ptr<T>(_: T) -> Option<T> where T: PartialEq;
+    // fn result_ptr<T>(_: T) -> Option<T> where T: PartialEq;
 }
 
 impl Form_Result for FormCode {
@@ -67,10 +69,10 @@ impl Form_Result for FormCode {
         }
     }
 
-    fn result_ptr<T>(ptr: T) -> Option<T> where T: PartialEq {
-        if ptr == ptr::null::<T>() { Some(ptr) }
-        else { None }
-    }
+    // fn result_ptr<T>(ptr: T) -> Option<T> where T: PartialEq {
+    //     if ptr == ptr::null::<T>() { Some(ptr) }
+    //     else { None }
+    // }
 }
 
 pub type FormResult = Result<(), FormCode>;
@@ -79,62 +81,78 @@ pub fn set_current_field(form: FORM, field: FIELD) -> FormResult
 { FormCode::result( unsafe { ll::set_current_field(form, field) } ) }
 
 pub fn current_field(form: FORM) -> Option<FIELD>
-{
-    let ptr = unsafe { ll::current_field(form) };
+{ unsafe { ll::current_field(form).as_mut().map(|x| {x as FIELD}) } }
 
-    if ptr == ptr::null_mut() {
-        Some(ptr)
-    }
-    else { None
-    }
-}
+pub fn unfocus_current_field(form: FORM) -> FormResult
+{ FormCode::result( unsafe { ll::unfocus_current_field(form) } ) }
 
-// pub fn unfocus_current_field(_:FORM) -> c_int
-// { unsafe {  } }
+pub fn set_form_page(form: FORM, n: i32) -> FormResult
+{ FormCode::result( unsafe { ll::set_form_page(form, n) } ) }
 
-// pub fn set_form_page(_:FORM, _:c_int) -> c_int { unsafe { ll:: } }
-// pub fn form_page(_:FORM) -> c_int { unsafe { ll:: } }
-// pub fn field_index(_:FIELD) -> c_int { unsafe { ll:: } }
+pub fn form_page(form: FORM) -> i32
+{ unsafe { ll::form_page(form) } }
+
+pub fn field_index(field: FIELD) -> i32
+{ unsafe { ll::field_index(field) } }
 
 
-// pub fn data_ahead(_:FORM) -> c_int { unsafe { ll:: } }
-// pub fn data_behind(_:FORM) -> c_int { unsafe { ll:: } }
+pub fn data_ahead(form: FORM) -> bool
+{ if unsafe { ll::data_ahead(form) } == 1 { true } else { false } }
+
+pub fn data_behind(form: FORM) -> bool
+{ if unsafe { ll::data_behind(form) } == 1 { true } else { false } }
 
 
-// pub fn new_field(_:c_int, _:c_int, _:c_int, _:c_int, _:c_int, _:c_int) -> FIELD { unsafe { ll:: } }
-// pub fn dup_field(_:FIELD, _:c_int, _:c_int) -> FIELD { unsafe { ll:: } }
-// pub fn link_field(_:FIELD, _:c_int, _:c_int) -> FIELD { unsafe { ll:: } }
-// pub fn free_field(_:FIELD) -> c_int { unsafe { ll:: } }
+pub fn new_field(height: i32, width: i32, toprow: i32, leftcol: i32, offscreen: i32, nbuffers: i32) -> Option<FIELD>
+{ unsafe { ll::new_field(height, width, toprow, leftcol, offscreen, nbuffers).as_mut().map(|x| {x as FIELD}) } }
+
+pub fn dup_field(field: FIELD, toprow: i32, leftcol: i32) -> Option<FIELD>
+{ unsafe { ll::dup_field(field, toprow, leftcol).as_mut().map(|x| {x as FIELD}) } }
+
+pub fn link_field(field: FIELD, toprow: i32, leftcol: i32) -> Option<FIELD>
+{ unsafe { ll::link_field(field, toprow, leftcol).as_mut().map(|x| {x as FIELD}) } }
+
+pub fn free_field(field: FIELD) -> FormResult
+{ FormCode::result( unsafe { ll::free_field(field) } ) }
 
 
-// pub fn field_info(_:FIELD, _:*mut c_int, _:*mut c_int, _:*mut c_int, _:*mut c_int, _:*mut c_int, _:*mut c_int) -> c_int { unsafe { ll:: } }
-// pub fn dynamic_field_info(_:FIELD, _:*mut c_int, _:*mut c_int, _:*mut c_int) -> c_int { unsafe { ll:: } }
+pub fn field_info(field: FIELD, rows: &mut i32, cols: &mut i32,
+                  frow: &mut i32, fcol: &mut i32, nrow: &mut i32, nbuf:&mut i32) -> FormResult
+{ FormCode::result( unsafe { ll::field_info(field, rows as *mut c_int,
+                                            cols as *mut c_int, frow as *mut c_int,
+                                            fcol as *mut c_int, nrow as *mut c_int,
+                                            nbuf as *mut c_int) } ) }
+
+pub fn dynamic_field_info(field: FIELD, rows: &mut i32, cols: &mut i32, max: &mut i32) -> FormResult
+{ FormCode::result( unsafe { ll::dynamic_field_info(field, rows as *mut c_int, cols as *mut c_int, max as *mut c_int) } ) }
 
 
 // // pub fn int set_field_type(FIELD *field, FIELDTYPE *type, ...) { unsafe { ll:: } } TODO
-// pub fn field_type(_:FIELD) -> FIELDTYPE { unsafe { ll:: } }
+//pub fn field_type(field: FIELD) -> FIELDTYPE { unsafe { ll:: } }
 // // pub fn void *field_arg(const FIELD *field) { unsafe { ll:: } } TODO
 
 
-// pub fn set_field_buffer(_:FIELD, _:c_int, _:*const c_char) -> c_int { unsafe { ll:: } }
+pub fn set_field_buffer(field: FIELD, buf: i32, value: &str) -> FormResult
+{ FormCode::result( unsafe { ll::set_field_buffer(field, buf, value.to_c_str().as_ptr()) } ) }
+
 // pub fn field_buffer(_:FIELD, _:c_int) -> *const c_char { unsafe { ll:: } }
-// pub fn set_field_status(_:FIELD, _:c_bool) -> c_int { unsafe { ll:: } }
+// pub fn set_field_status(_:FIELD, _:c_bool) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 // pub fn field_status(_:FIELD) -> c_bool { unsafe { ll:: } }
-// pub fn set_max_field(_:FIELD, _:c_int) -> c_int { unsafe { ll:: } }
+// pub fn set_max_field(_:FIELD, _:c_int) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 
 
-// pub fn set_form_fields(_:FORM, _:*mut FIELD) -> c_int { unsafe { ll:: } }
+// pub fn set_form_fields(_:FORM, _:*mut FIELD) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 // pub fn form_fields(_:FORM) -> *mut FIELD { unsafe { ll:: } }
-// pub fn field_count(_:FORM) -> c_int { unsafe { ll:: } }
-// pub fn move_field(_:FIELD, _:c_int, _:c_int) -> c_int { unsafe { ll:: } }
+// pub fn field_count(_:FORM) -> FormResult { FormCode::result( unsafe { ll:: } ) }
+// pub fn move_field(_:FIELD, _:c_int, _:c_int) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 
 
-// pub fn set_field_fore(_:FIELD, _:chtype) -> c_int { unsafe { ll:: } }
+// pub fn set_field_fore(_:FIELD, _:chtype) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 // pub fn field_fore(_:FIELD) -> chtype { unsafe { ll:: } }
-// pub fn set_field_back(_:FIELD, _:chtype) -> c_int { unsafe { ll:: } }
+// pub fn set_field_back(_:FIELD, _:chtype) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 // pub fn field_back(_:FIELD) -> chtype { unsafe { ll:: } }
-// pub fn set_field_pad(_:FIELD, _:c_int) -> c_int { unsafe { ll:: } }
-// pub fn field_pad(_:FIELD) -> c_int { unsafe { ll:: } }
+// pub fn set_field_pad(_:FIELD, _:c_int) -> FormResult { FormCode::result( unsafe { ll:: } ) }
+// pub fn field_pad(_:FIELD) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 
 
 // // TODO
@@ -148,41 +166,41 @@ pub fn current_field(form: FORM) -> Option<FIELD>
 // // pub fn Form_Hook form_term(const FORM *form) { unsafe { ll:: } }
 
 
-// pub fn set_field_just(_:FIELD, _:c_int) -> c_int { unsafe { ll:: } }
-// pub fn field_just(_:FIELD) -> c_int { unsafe { ll:: } }
+// pub fn set_field_just(_:FIELD, _:c_int) -> FormResult { FormCode::result( unsafe { ll:: } ) }
+// pub fn field_just(_:FIELD) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 
 
-// pub fn set_field_opts(_:FIELD, _:FieldOptions) -> c_int { unsafe { ll:: } }
-// pub fn field_opts_on(_:FIELD, _:FieldOptions) -> c_int { unsafe { ll:: } }
-// pub fn field_opts_off(_:FIELD, _:FieldOptions) -> c_int { unsafe { ll:: } }
+// pub fn set_field_opts(_:FIELD, _:FieldOptions) -> FormResult { FormCode::result( unsafe { ll:: } ) }
+// pub fn field_opts_on(_:FIELD, _:FieldOptions) -> FormResult { FormCode::result( unsafe { ll:: } ) }
+// pub fn field_opts_off(_:FIELD, _:FieldOptions) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 // pub fn field_opts(_:FIELD) -> FieldOptions { unsafe { ll:: } }
 
 
-// pub fn form_driver(_:FORM, _:c_int) -> c_int { unsafe { ll:: } }
-// // pub fn form_driver_w(_:FORM, _:c_int, wchar_t wch) -> c_int { unsafe { ll:: } } TODO wchar ?
+// pub fn form_driver(_:FORM, _:c_int) -> FormResult { FormCode::result( unsafe { ll:: } ) }
+// // pub fn form_driver_w(_:FORM, _:c_int, wchar_t wch) -> FormResult { FormCode::result( unsafe { ll:: } ) } TODO wchar ?
 
 
-// pub fn set_form_opts(_:FORM, _:FieldOptions) -> c_int { unsafe { ll:: } }
-// pub fn form_opts_on(_:FORM, _:FieldOptions) -> c_int { unsafe { ll:: } }
-// pub fn form_opts_off(_:FORM, _:FieldOptions) -> c_int { unsafe { ll:: } }
+// pub fn set_form_opts(_:FORM, _:FieldOptions) -> FormResult { FormCode::result( unsafe { ll:: } ) }
+// pub fn form_opts_on(_:FORM, _:FieldOptions) -> FormResult { FormCode::result( unsafe { ll:: } ) }
+// pub fn form_opts_off(_:FORM, _:FieldOptions) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 // pub fn form_opts(_:FORM) -> FieldOptions { unsafe { ll:: } }
 
 
 // pub fn form_request_name(_:c_int) -> *const c_char { unsafe { ll:: } }
-// pub fn form_request_by_name(_:*const c_char) -> c_int { unsafe { ll:: } }
+// pub fn form_request_by_name(_:*const c_char) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 
 
-// pub fn set_form_win(_:FORM, _:WINDOW) -> c_int { unsafe { ll:: } }
+// pub fn set_form_win(_:FORM, _:WINDOW) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 // pub fn form_win(_:FORM) -> WINDOW { unsafe { ll:: } }
-// pub fn set_form_sub(_:FORM, _:WINDOW) -> c_int { unsafe { ll:: } }
+// pub fn set_form_sub(_:FORM, _:WINDOW) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 // pub fn form_sub(_:FORM) -> WINDOW { unsafe { ll:: } }
-// pub fn scale_form(_:FORM, _:*mut c_int, _:*mut c_int) -> c_int { unsafe { ll:: } }
+// pub fn scale_form(_:FORM, _:*mut c_int, _:*mut c_int) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 
 
 // pub fn new_field(_:c_int, _:c_int, _:c_int, _:c_int, _:c_int, _:c_int) -> FIELD { unsafe { ll:: } }
 // pub fn dup_field(_:FIELD, _:c_int, _:c_int) -> FIELD { unsafe { ll:: } }
 // pub fn link_field(_:FIELD, _:c_int, _:c_int) -> FIELD { unsafe { ll:: } }
-// pub fn free_field(_:FIELD) -> c_int { unsafe { ll:: } }
+// pub fn free_field(_:FIELD) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 
 
 // // TODO
@@ -200,18 +218,18 @@ pub fn current_field(form: FORM) -> Option<FIELD>
 
 
 // pub fn new_form(_:*mut FIELD) -> FORM { unsafe { ll:: } }
-// pub fn free_form(_:FORM) -> c_int { unsafe { ll:: } }
+// pub fn free_form(_:FORM) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 
 
-// pub fn set_new_page(_:FIELD, _:c_bool) -> c_int { unsafe { ll:: } }
+// pub fn set_new_page(_:FIELD, _:c_bool) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 // pub fn new_page(_:FIELD) -> c_bool { unsafe { ll:: } }
 
 
-// pub fn pos_form_cursor(_:FORM) -> c_int { unsafe { ll:: } }
+// pub fn pos_form_cursor(_:FORM) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 
 
-// pub fn post_form(_:FORM) -> c_int { unsafe { ll:: } }
-// pub fn unpost_form(_:FORM) -> c_int { unsafe { ll:: } }
+// pub fn post_form(_:FORM) -> FormResult { FormCode::result( unsafe { ll:: } ) }
+// pub fn unpost_form(_:FORM) -> FormResult { FormCode::result( unsafe { ll:: } ) }
 
 
 
